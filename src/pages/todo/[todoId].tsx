3e56865dc87efddaef97027204ee6efbb1ddd4a2
Anthony Catullo <anthonycatullo@oatslam.local>
2023-06-07 16:26:20 -0500

    background and rendered content

--- src/pages/todo/[todoId].tsx
+++ src/pages/todo/[todoId].tsx
@@ -8,7 +8,14 @@ const SingleTodoPage: NextPage = () => {
 
   const { data, isLoading, isError } = api.todo.getOne.useQuery({ todoId });
 
-  return <div>Single Page {data?.content}</div>;
+  return (
+    <main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+        <div className="flex flex-col items-center gap-2"></div>
+        <p className="text-white">{data?.content}</p>
+      </div>
+    </main>
+  );
 };
 
 export default SingleTodoPage;
