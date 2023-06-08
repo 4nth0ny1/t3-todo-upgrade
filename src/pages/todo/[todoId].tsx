80b0f546b353fd0e3245f63ce5b9c2e396559f17
Anthony Catullo <anthonycatullo@oatslam.local>
2023-06-07 21:45:43 -0500

    added styles to todos

--- src/pages/todo/[todoId].tsx
+++ src/pages/todo/[todoId].tsx
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import { api } from "../../utils/api";
 import { type NextPage } from "next";
+import Link from "next/link";
 
 const SingleTodoPage: NextPage = () => {
   const router = useRouter();
@@ -8,12 +9,19 @@ const SingleTodoPage: NextPage = () => {
 
   const { data, isLoading, isError } = api.todo.getOne.useQuery({ todoId });
 
+  if (isLoading) return <div>Loading...</div>;
+
+  if (isError) return <div>Something went wrong ...</div>;
+
   return (
     <main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
         <div className="flex flex-col items-center gap-2"></div>
         <p className="text-white">{data?.content}</p>
       </div>
+      <Link href="/" className="btn-info btn-sm btn">
+        back
+      </Link>
     </main>
   );
 };
